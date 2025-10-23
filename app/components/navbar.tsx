b7c3f9eb847d9d7db2bdd3996c85e3763ab9a003
Trevin Jones <tr3vinj@gmail.com>
2025-10-22 19:16:06 -0600

    add navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,9 +1,22 @@
 import { faBars } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useState } from "react"
+import { NavLink } from "react-router"
 
 export function Navbar() {
-  return <nav className="w-full bg-madds-pink justify-center grid grid-cols-3">
-    <FontAwesomeIcon icon={faBars} className="text-2xl p-2 my-auto" />
-    <img src="mbm.png" className="w-60 mx-auto py-2 my-auto" />
-  </nav>
+  const [hidden, setHidden] = useState(true);
+
+  const navClassName = `bg-madds-pink px-2 text-xl font-medium ${hidden ? "hidden" : ""}`
+
+  return <>
+    <div className="w-full bg-madds-pink grid grid-cols-3">
+      <FontAwesomeIcon onClick={() => setHidden(!hidden)} icon={faBars} className="text-2xl p-2 my-auto" />
+      <img src="mbm.png" className="w-60 mx-auto py-2 my-auto" />
+    </div>
+    <nav className={navClassName}>
+      <NavLink to="/">
+        Home
+      </NavLink>
+    </nav>
+  </>
 }
